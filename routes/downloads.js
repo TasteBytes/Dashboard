9cc9b853a4b1fe544f64bb94800a745e4b12ddac
Daniel Stansberry <stansberry2295@gmail.com>
2017-04-11 16:03:45 -0700

    added support for authenticated layout

--- routes/downloads.js
+++ routes/downloads.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../user_service');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  var signedIn = false;
+  if (userService.firebase.auth().currentUser != null){
+      signedIn = true;
+  }
     res.render('downloads', {
+        signedIn: signedIn,
         title: 'TasteBytes - Downloads',
         styles: ['index.css'],
         javascript: ['dropdown.js']
