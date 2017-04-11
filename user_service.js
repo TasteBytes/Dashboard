6986493ce71fe76e38f62096cc80a95e4665cba8
Daniel Stansberry <stansberry2295@gmail.com>
2017-04-11 16:05:37 -0700

    indentation changes

--- user_service.js
+++ user_service.js
@@ -1,54 +1,61 @@
 const firebase = require('firebase');
 
 const config = {
-  apiKey: "AIzaSyBrJDvsp_4dcQ8J5YJrFfQwI3-xHQnKGjs",
-  authDomain: "tastebytes-e421e.firebaseapp.com",
-  databaseURL: "https://tastebytes-e421e.firebaseio.com",
-  projectId: "tastebytes-e421e",
-  storageBucket: "tastebytes-e421e.appspot.com",
-  messagingSenderId: "240649605064"
+    apiKey: "AIzaSyBrJDvsp_4dcQ8J5YJrFfQwI3-xHQnKGjs",
+    authDomain: "tastebytes-e421e.firebaseapp.com",
+    databaseURL: "https://tastebytes-e421e.firebaseio.com",
+    projectId: "tastebytes-e421e",
+    storageBucket: "tastebytes-e421e.appspot.com",
+    messagingSenderId: "240649605064"
 };
 
-firebaseRef = firebase.initializeApp(config);
+firebase.initializeApp(config);
 const auth = firebase.auth();
 const rootRef = firebase.database().ref();
-var signedIn = 'asdf';
-var userID = null;
 
 function createUser(email, password, callback) {
-  auth.createUserWithEmailAndPassword(email, password).then(function(success) {
-    callback(success.code)
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    callback(error);
-  });
+    auth.createUserWithEmailAndPassword(email, password).then(function(success) {
+        callback(success.code)
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        callback(error);
+    });
 }
 
 function signInUser(email, password, callback) {
-  firebase.auth().signInWithEmailAndPassword(email, password).then(function(success) {
-    signedIn = true;
-    callback(success.code);
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(success) {
+        callback(success.code);
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        callback(error);
+    });
+}
+
+function signOutUser(callback){
+  firebase.auth().signOut().then(function(success) { 
+    // Sign-out successful.
+    callback(success);
   }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    callback(error);
+    // An error happened.
+    callback(error); 
   });
 }
 
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log('user is signed in');
-  } else{
-    console.log('user is not signed in');
-  }
+    if (user) {
+        console.log('user is signed in');
+    } else {
+        console.log('user is not signed in');
+    }
 });
 
 module.exports = {
-
-	addUser : createUser,
-	authenticate : signInUser
-
+    addUser: createUser,
+    authenticate: signInUser,
+    firebase: firebase,
+    signOut: signOutUser
 }
