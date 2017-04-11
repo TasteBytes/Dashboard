fa40937da6dbb471a1625d4e4f4642d75a838899
Daniel Stansberry <stansberry2295@gmail.com>
2017-04-11 16:03:59 -0700

    added support for authenticated layout

--- routes/features.js
+++ routes/features.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../user_service');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('features', {
+  var signedIn = false;
+  if (userService.firebase.auth().currentUser != null){
+      signedIn = true;
+  }
+      res.render('features', {
+        signedIn: signedIn,
         title: 'TasteBytes - Features',
         styles: ['index.css'],
     });
