1e71eff240398469bc0c9ea23ca0371763802ceb
Daniel Stansberry <stansberry2295@gmail.com>
2017-04-13 16:19:54 -0700

    now using user-agent to get the OS of the client

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,31 @@ var express = require('express');
 var router = express.Router();
 var os = require('os');
 var userService = require('../user_service');
+var useragent = require('useragent');
 
-var getHostOS = (() => {
-    var type = os.type();
+
+var getHostOS = ((agent) => {
+    var osType = agent.os.toString();
+    if (osType.includes('Ubuntu')) {
+      var os = 'Linux'
+      var osRelease = ''
+    } else if (osType.includes('Mac OS')){
+      var os = 'Mac'
+      var osRelease = ''
+    } else if (osType.includes('Windows')){
+      var os = 'Windows'
+      var osRelease = ''
+    }
     return {
-        os: type,
-        download: `https://github.com/${type}/downloadLink`
+        os: os,
+        download: osRelease
     }
 });
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var hostOS = getHostOS();
+    var hostOS = getHostOS(useragent.parse(req.headers['user-agent']));
     var signedIn = false;
     if (userService.firebase.auth().currentUser != null) {
         signedIn = true;
