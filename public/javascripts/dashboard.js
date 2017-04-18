7b470429f1f126fc2c83f4964082e9573635af72
DanRooney <danielerooney@gmail.com>
2017-04-18 16:15:22 -0700

    created modal for adding menu items

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -7,12 +7,21 @@ $('.context.example .ui.sidebar')
 ;
 
 $(document).ready(function(){
-    
-    
     $('.secondary.menu .item').tab({history:false});
     $('.tabular.menu .item').tab({history:false});
 });
-
-
 ;
 
+function addItem(){
+    $('.ui.modal').modal('show');
+}
+
+function createAccordion(){
+    $( '.ui button' ).before( 
+        "<p>accordion goes here</p>" 
+    );
+}
+
+function closeModal(){
+    $('.ui.modal').modal('hide');
+}
