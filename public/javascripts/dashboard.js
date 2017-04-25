080fd6fb42c1896153d5f64bfacc7261b5bb7a83
Daniel Stansberry <stansberry2295@gmail.com>
2017-04-24 19:19:34 -0700

    change for deleteItem function

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,56 +1,70 @@
 // using context
 $('.context.example .ui.sidebar')
-  .sidebar({
-    context: $('.context.example .bottom.segment')
-  })
-  .sidebar('attach events', '.context.example .menu .item')
-;
+    .sidebar({
+        context: $('.context.example .bottom.segment')
+    })
+    .sidebar('attach events', '.context.example .menu .item');
 
-$(document).ready(function(){
-    $('.secondary.menu .item').tab({history:false});
-    $('.tabular.menu .item').tab({history:false});
-});
-;
+$(document).ready(function() {
+    $('.secondary.menu .item').tab({
+        history: false
+    });
+    $('.tabular.menu .item').tab({
+        history: false
+    });
+});;
 
-$(document).ready(function(){
+$(document).ready(function() {
     $('.ui.accordion').accordion();
 });
 
 //find out who called us and update CategoryModal with path info
-function addCategory(category){
-    var path= category.getAttribute("data-ref")
-    $('#categoryModalInput').attr('value',path);
+function addCategory(category) {
+    var path = category.getAttribute("data-ref")
+    $('#categoryModalInput').attr('value', path);
     $('#CategoryModal').modal('show');
 }
-function saveCategory(){
+
+function saveCategory() {
 
 }
 
-function addMenu(){
+function addMenu() {
     $('#MenuModal').modal('show');
 }
-function saveMenu(){
+
+function saveMenu() {
 
 }
 
-function addEntree(entree){
+function addEntree(entree) {
     var path = entree.getAttribute("data-ref");
     $('#entreeModalInput').attr('value', path);
     $('#EntreeModal').modal('show');
 }
-function saveEntree(){
+
+function saveEntree() {
 
 }
-function editEntree(){
-    console.log("edit");
+
+function editEntree() {
     event.stopPropagation();
+    console.log("edit");
     $('#EditModal').modal('show');
 }
-function deleteEntree(Entree){
-    console.log("delete");
+
+function deleteItem(item) {
     event.stopPropagation();
+    var path = item.getAttribute("data-ref");
+    $('#deleteText').text(`Are you sure you would like to delete ${path}`)
+    $('#DeleteModal').modal({
+            onApprove: function() {
+                window.alert('Make POST request here!');
+            }
+        })
+        .modal('show');
 }
 
-function closeModal(){
+function closeModal() {
     $('.ui.modal').modal('hide');
 }
