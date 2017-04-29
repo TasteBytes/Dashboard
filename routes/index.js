b0b3e9aa3e0e813b5ec87a925c62be4d65b5f5fb
Daniel Stansberry <stansberry2295@gmail.com>
2017-04-29 15:19:15 -0700

    index does not handle theOS type anymore

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var os = require('os');
 var userService = require('../user_service');
-var useragent = require('useragent');
-
-var getHostOS = ((agent) => {
-    var osType = agent.os.toString();
-    if (osType.includes('Ubuntu')) {
-      var os = 'Linux'
-      var osRelease = ''
-    } else if (osType.includes('Mac OS')){
-      var os = 'Mac'
-      var osRelease = ''
-    } else if (osType.includes('Windows')){
-      var os = 'Windows'
-      var osRelease = ''
-    }
-    return {
-        os: os,
-        download: osRelease
-    }
-});
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var hostOS = getHostOS(useragent.parse(req.headers['user-agent']));
     var signedIn = false;
     if (userService.firebase.auth().currentUser != null) {
         signedIn = true;
@@ -36,7 +14,6 @@ router.get('/', function(req, res, next) {
         title: 'TasteBytes',
         styles: ['index.css'],
         javascript: ['menus.js'],
-        hostOS: hostOS
     });
 });
 
