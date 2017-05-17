26a4a0274544fc579f9b8e7daea7eab528ef082b
Dan Rooney <danielerooney@gmail.com>
2017-05-17 12:02:53 -0700

    made settings template

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -79,3 +79,39 @@ function deleteEntree(entree) {
 function closeModal() {
     $('.ui.modal').modal('hide');
 }
+
+//javascript for the settings page
+$('.field.example form')
+  .form({
+    on: 'blur',
+    fields: {
+      empty: {
+        identifier  : 'empty',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a value'
+          }
+        ]
+      },
+      dropdown: {
+        identifier  : 'dropdown',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please select a dropdown value'
+          }
+        ]
+      },
+      checkbox: {
+        identifier  : 'checkbox',
+        rules: [
+          {
+            type   : 'checked',
+            prompt : 'Please check the checkbox'
+          }
+        ]
+      }
+    }
+  })
+;
