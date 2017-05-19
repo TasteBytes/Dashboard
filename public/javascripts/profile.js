30f8cd8ea65aea6bd1eb823133bb86b63aadb996
Daniel Stansberry <stansberry2295@gmail.com>
2017-05-18 21:22:41 -0700

    split up the js files

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
     $(".rating").rating();
+    $('#profile-image')
+      .dimmer({
+        on: 'hover'
+      });
 });;
--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-    $('#example3').calendar({
-      type: 'time'
+  $('#example3').calendar({
+    type: 'time'
+  });
+  $('#profile-image')
+    .dimmer({
+      on: 'hover'
     });
 });
