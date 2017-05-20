44b308134e0232cc7be0cb6c4535530d43257b0a
Daniel Stansberry <stansberry2295@gmail.com>
2017-05-19 19:49:26 -0700

    auto submit image when uploaded

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,7 +1,13 @@
+var control;
+
 $(document).ready(function() {
-    $(".rating").rating();
-    $('#profile-image')
-      .dimmer({
-        on: 'hover'
-      });
-});;
+  $(".rating").rating();
+  $('#profile-image')
+    .dimmer({
+      on: 'hover'
+    });
+  control = document.getElementById("profile-input");
+  control.addEventListener("change", function(event) {
+      document.getElementById("uploadProfileForm").submit();
+  }, false);
+});
